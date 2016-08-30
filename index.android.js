d11258d5a2a39eb0b3592a4920b9f0c52976291e
Nat Weerawan <nat@cmmc.io>
2016-08-30 19:04:12 +0700

    Complete demo.

--- index.android.js
+++ index.android.js
@@ -4,50 +4,84 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View
 } from 'react-native';
 
+let MicroGear = require('react-native-cmmc-microgear');
+
 class netpie_react_native_demo extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
+
+    APPID = "CMMC";
+    KEY = "Qiah96b7mdcmtvq";
+    SECRET = "bt81P9VyTilLkdMJrN9c10Jph";
+
+    constructor() {
+        super();
+        this.state = {
+            msg: 'Connecting..',
+            topic: ''
+        };
+
+        this.microgear = MicroGear.create({
+            key: this.KEY,
+            secret: this.SECRET
+        });
+
+        this.microgear.on('connected', () => {
+            console.log('Connected...');
+            this.microgear.subscribe("/CMMC/gearname/+/temp");
+            // setInterval(() => {
+            //     this.microgear.chat('mygear', 'Hello world.');
+            // }, 400);
+        });
+
+        this.microgear.on('message', (topic, body) => {
+            // console.log('incoming : ' + topic + ' : ' + body);
+            this.setState({msg: body});
+        });
+
+        this.microgear.on('closed', () => {
+            console.log('Closed...');
+        });
+
+        this.microgear.on("error", (reason) => {
+            console.log("reason = ", reason);
+            this.setState({msg: reason});
+        });
+    }
+
+    componentDidMount() {
+        this.microgear.connect(this.APPID);
+        console.log("DID MOUNT");
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.dataText}>
+                    {this.state.msg }
+                </Text>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    dataText: {
+        fontSize: 80,
+        textAlign: 'center',
+    },
 });
 
 AppRegistry.registerComponent('netpie_react_native_demo', () => netpie_react_native_demo);
